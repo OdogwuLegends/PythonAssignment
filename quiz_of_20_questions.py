42aa64d4644eb28cad263ed6ee5a2fef93e1df12
OdogwuLegends <119644231+OdogwuLegends@users.noreply.github.com>
2023-01-22 23:43:11 -0800

    Add files via upload

---
+++ quiz_of_20_questions.py
@@ -0,0 +1,318 @@
+#user_choice = int(input("Enter "))
+counter = 1
+while counter <= 20
+
+#1
+	user_response = int(input("What is the capital of Canada?\n" \
+		" 1. Ottawa\n" \
+		" 2. Ontario\n"))
+
+	if user_response == 1:
+			print ("Correct!")
+	elif user_response == 2:
+		print ("Incorrect")
+	else:
+		print ("Invalid input.")
+
+
+#2
+	user_response = int(input("How many continents do we have?\n" \
+		" 1. 9\n" \
+		" 2. 8\n"))
+
+
+	if user_response == 1:
+		print ("Incorrect!")
+	else:
+		print ("Correct")
+	else:
+		print ("Invalid input.")
+
+
+#3
+	user_response = int(input("The sum of 78 and 98 is?\n" \
+		" 1. 159\n" \
+		" 2. 176\n"))
+
+
+	if user_response == 1:
+		print ("Incorrect!")
+	elif:
+		print ("Correct")
+	else:
+		print ("Invalid input.")
+
+
+#4
+	user_response = int(input("What year did Nigeria gain independence?\n" \
+		" 1. 1960\n" \
+		" 2. 1963\n"))
+
+	if user_response == 1:
+		print ("Correct!")
+	elif:
+		print ("Incorrect")
+	else:
+		print ("Invalid input.")
+
+
+#5
+	user_response = int(input("Where would you find the Spanish steps?\n" \
+		" 1. Spain\n" \
+		" 2. Italy\n"))
+
+	if user_response == 1:
+		print ("Incorrect!")
+	elif:
+		print ("Correct")
+	else:
+		print ("Invalid input.")
+
+
+#6
+user_response = int(input("Who was the first military Head of State in Nigeria?\n" \
+	" 1. J.T. Aguiyi-Ironsi\n" \
+	" 2. Yakubu Gowon\n"))
+
+if user_response == 1:
+	if user_response == 1:
+		print ("Correct!")
+	else:
+		print ("Incorrect")
+else:
+	print ("Invalid input.")
+
+
+#7
+user_response = int(input("What continent would you find Madagascar?\n" \
+	" 1. Africa\n" \
+	" 2. Asia\n"))
+
+if user_response == 1:
+	if user_response == 1:
+		print ("Correct!")
+	else:
+		print ("Incorrect")
+else:
+	print ("Invalid input.Try again")
+
+
+#8
+user_response = int(input("Which of these are primary colours?\n" \
+	" 1. Yellow,Red,Green\n" \
+	" 2. Red,Yellow,Blue\n"))
+
+if user_response == 1:
+	if user_response == 1:
+		print ("Incorrect!")
+	else:
+		print ("Correct")
+else:
+	print ("Invalid input.Try again")
+
+#9
+user_response = int(input("Damaturu is the capital of which Nigerian state?\n" \
+	" 1. Taraba\n" \
+	" 2. Yobe\n"))
+
+if user_response == 1:
+	if user_response == 1:
+		print ("Incorrect!")
+	else:
+		print ("Correct")
+else:
+	print ("Invalid input.Try again")
+
+#10
+user_response = int(input("Planet Earth is in what galaxy?\n" \
+	" 1. Milky way\n" \
+	" 2. Andromeda\n"))
+
+if user_response == 1:
+	if user_response == 1:
+		print ("Correct!")
+	else:
+		print ("Incorrect")
+else:
+	print ("Invalid input.Try again")
+
+
+#11
+user_response = int(input("Who played the character 'Legolas' in the Lord of The Rings?\n" \
+	" 1. Orlando Bloom\n" \
+	" 2. Elijah Wood\n"))
+
+if user_response == 1:
+	if user_response == 1:
+		print ("Correct!")
+	else:
+		print ("Incorrect")
+else:
+	print ("Invalid input.Try again")
+
+
+#12
+user_response = int(input("The daughter of your sister is your..?\n" \
+	" 1. Cousin\n" \
+	" 2. Niece\n"))
+
+if user_response == 1:
+	if user_response == 1:
+		print ("Incorrect!")
+	else:
+		print ("Correct")
+else:
+	print ("Invalid input.Try again")
+
+
+#13
+user_response = int(input("Obstetrics is a branch of medicine particularly concerned with what?\n" \
+	" 1. Pregnancy and childbirth\n" \
+	" 2. Heart conditions\n"))
+
+if user_response == 1:
+	if user_response == 1:
+		print ("Correct!")
+	else:
+		print ("Incorrect")
+else:
+	print ("Invalid input.Try again")
+
+
+#14
+user_response = int(input("The Eiffel tower is in which city?\n" \
+	" 1. Tokyo\n" \
+	" 2. Paris\n"))
+
+if user_response == 1:
+	if user_response == 1:
+		print ("Incorrect!")
+	else:
+		print ("Correct")
+else:
+	print ("Invalid input.")
+
+
+#15
+user_response = int(input("In Nigeria, the abbreviation NHIS stands for National what Service?\n" \
+	" 1. Honors\n" \
+	" 2. Health\n"))
+
+if user_response == 1:
+	if user_response == 1:
+		print ("Incorrect!")
+	else:
+		print ("Correct")
+else:
+	print ("Invalid input.")
+
+
+#16
+user_response = int(input("The smallest country in the world is?\n" \
+	" 1. Vatican\n" \
+	" 2. Mauritius\n"))
+
+if user_response == 1:
+	if user_response == 1:
+		print ("Correct!")
+	else:
+		print ("Incorrect")
+else:
+	print ("Invalid input.")
+
+
+#17
+user_response = int(input("The Giza Pyramids are found in?\n" \
+	" 1. Greece\n" \
+	" 2. Egypt\n"))
+
+if user_response == 1:
+	if user_response == 1:
+		print ("Incorrect!")
+	else:
+		print ("Correct")
+else:
+	print ("Invalid input.")
+
+
+#18
+user_response = int(input("Which country would host the 2028 Olympics games?\n" \
+	" 1. Los Angeles\n" \
+	" 2. Oslo\n"))
+
+if user_response == 1:
+	print ("Correct!")
+elif user_response == 2:
+	print ("Incorrect")
+else:
+	print ("Invalid input.")
+
+
+#19
+user_response = int(input("Who won the first ever World Cup?\n" \
+	" 1. Brazil\n" \
+	" 2. Uruguay\n"))
+
+if user_response == 1:
+	print ("Correct!")
+elif user_response == 2:
+	print ("Incorrect")
+else:
+	print ("Invalid input.")
+
+
+#20
+user_response = int(input("The first iPhone was released in what year?\n" \
+	" 1. 2009\n" \
+	" 2. 2007\n"))
+
+if user_response == 1:
+	print ("Incorrect!")
+elif user_response == 2:
+	print ("Correct")
+else:
+	print ("Invalid input.")
+
+
+#21
+user_response = int(input("Which of the following refers to the word fire?\n" \
+	" 1. Tornado\n" \
+	" 2. Inferno\n"))
+
+if user_response == 1:
+	print ("Correct!")
+elif user_response == 2:
+	print ("Incorrect")
+else:
+	print ("Invalid input.")
+
+
+#22
+user_response = int(input("Roland Garros stadium is in which city?\n" \
+	" 1. Paris\n" \
+	" 2. Inferno\n"))
+
+if user_response == 1:
+	print ("Correct!")
+elif user_response == 2:
+	print ("Incorrect")
+else:
+	print ("Invalid input.")
+
+
+#23
+user_response = int(input("Which of these hold bones together at the joints of the body?\n" \
+	" 1. Paris\n" \
+	" 2. Inferno\n"))
+
+if user_response == 1:
+	print ("Correct!")
+elif user_response == 2:
+	print ("Incorrect")
+else:
+	print ("Invalid input.")
+
+	counter += 1
+
+
+
